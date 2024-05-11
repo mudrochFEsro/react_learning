e1be3b1f5d203204fdba2252044adb6d13cce3ba
Mudroch.FE s.r.o. <wanrltv3328@gmail.com>
2024-05-11 18:51:41 +0200

    update

---
+++ state/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Game from './Game'
+import Rando from './Rando'
+import Button from "./Button";
+import BrokenClick from "./BrokenClick";
+
+function App() {
+  return (
+    <div className="App">
+      <BrokenClick/>
+    </div>
+  );
+}
+
+export default App;
