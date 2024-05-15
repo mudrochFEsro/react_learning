cc120f4107c1fe8b659ecbf80de1b4cba206c70d
Mudroch.FE s.r.o. <wanrltv3328@gmail.com>
2024-05-15 16:02:41 +0200

    update

--- roll_dice/src/RollDice.js
+++ roll_dice/src/RollDice.js
@@ -43,7 +43,10 @@ class RollDice extends Component {
                     <Die face={this.state.die1} rolling={this.state.isRolling}/>
                     <Die face={this.state.die2} rolling={this.state.isRolling}/>
                 </div>
-                <button onClick={this.roll} disabled={this.state.isRolling} className={this.state.isRolling && 'disabled'}>
+                <button
+                    onClick={this.roll}
+                    disabled={this.state.isRolling}
+                    className={this.state.isRolling && 'RollDice-button-disabled'}>
                     {this.state.isRolling ? 'Rolling...' : 'Roll Dice!'}
                 </button>
             </div>
